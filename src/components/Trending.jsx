38a34c82db2ed0d6a3d15aa6b0d677a255b593bf
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2025-01-04 04:39:58 +0530

    created trending body poster and title

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,14 +1,34 @@
 import {useNavigate} from "react-router-dom";
 import Topnav from "../partials/Topnav.jsx";
 import Dropdown from "../partials/Dropdown.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "../utils/axios.jsx";
+import Cards from "../partials/Cards.jsx";
 
 function Trending() {
     const navigate = useNavigate();
     const [category, setCategory] = useState("all");
-    return (
-        <div className="p-[3%] w-screen h-screen">
-            <div className="w-full flex items-center">
+    const [duration, setDuration] = useState("day");
+    const [trending, setTrending] = useState([]);
+
+    const getTrending = async () => {
+        try {
+            const {data} = await axios.get(`/trending/${category}/${duration}`);
+            setTrending(data.results);
+            console.log(data.results);
+        }
+        catch (error) {
+            console.log("Error in Cards Fetching",error);
+        }
+    }
+
+    useEffect(() => {
+        getTrending();
+    },[category, duration])
+
+    return trending.length > 0 ? (
+        <div className="p-[2%] w-screen h-screen overflow-y-auto">
+            <div className="w-full flex items-center h-[8vh]">
                 <h1 className="w-[20%] font-semibold text-2xl text-zinc-400">
                     <i className="hover:text-[#6556cd] ri-arrow-left-long-line pr-3"
                        onClick={() => navigate(-1)}>
@@ -16,12 +36,15 @@ function Trending() {
                 </h1>
                 <div className="flex items-center justify-between w-[80%]">
                     <Topnav/>
-                    <Dropdown title="Filter" options={["tv", "movie", "all"]} func={(e)=>setCategory(e.target.value)}/>
-                    <Dropdown title="Duration" options={["day", "week"]} func={(e)=>setCategory(e.target.value)}/>
+                    <Dropdown title="Category" options={["tv", "movie", "all"]} func={(e)=>setCategory(e.target.value)}/>
+                    <Dropdown title="Duration" options={["day", "week"]} func={(e)=>setDuration(e.target.value)}/>
                 </div>
             </div>
+            <div className="w-full h-[92vh]">
+                <Cards data={trending}/>
+            </div>
         </div>
-    )
+    ) : <h1>Loading......</h1>;
 }
 
 export default Trending;
