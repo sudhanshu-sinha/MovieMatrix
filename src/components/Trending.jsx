0eb798a89f354307072ed4a06879910a59fcfe93
Sudhanshu Sinha <sudhanshu.cu@gmail.com>
2025-01-04 07:19:22 +0530

    added infinite scroll in Trending page

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -4,18 +4,21 @@ import Dropdown from "../partials/Dropdown.jsx";
 import {useEffect, useState} from "react";
 import axios from "../utils/axios.jsx";
 import Cards from "../partials/Cards.jsx";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function Trending() {
     const navigate = useNavigate();
     const [category, setCategory] = useState("all");
     const [duration, setDuration] = useState("day");
     const [trending, setTrending] = useState([]);
+    const [page, setPage] = useState(1);
 
     const getTrending = async () => {
         try {
             const {data} = await axios.get(`/trending/${category}/${duration}`);
-            setTrending(data.results);
-            console.log(data.results);
+            setTrending(([...trending, ...data.results]));
+            setPage(page + 1);
+            console.log(data);
         }
         catch (error) {
             console.log("Error in Cards Fetching",error);
@@ -27,8 +30,8 @@ function Trending() {
     },[category, duration])
 
     return trending.length > 0 ? (
-        <div className="p-[2%] w-screen h-screen overflow-y-auto">
-            <div className="w-full flex items-center h-[8vh]">
+        <div className="w-screen h-screen">
+            <div className="px-[1%] pt-8 w-full flex items-center h-[8vh]">
                 <h1 className="w-[20%] font-semibold text-2xl text-zinc-400">
                     <i className="hover:text-[#6556cd] ri-arrow-left-long-line pr-3"
                        onClick={() => navigate(-1)}>
@@ -40,9 +43,14 @@ function Trending() {
                     <Dropdown title="Duration" options={["day", "week"]} func={(e)=>setDuration(e.target.value)}/>
                 </div>
             </div>
-            <div className="w-full h-[92vh]">
+            <InfiniteScroll
+            dataLength={trending.length}
+            next={getTrending}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            >
                 <Cards data={trending}/>
-            </div>
+            </InfiniteScroll>
         </div>
     ) : <h1>Loading......</h1>;
 }
--- src/partials/Cards.jsx
+++ src/partials/Cards.jsx
@@ -2,9 +2,9 @@ import {Link} from "react-router-dom";
 
 function Cards({data}){
     return(
-        <div className="cards flex flex-wrap w-full gap-x-11">
+        <div className="p-[2.37%] cards flex flex-wrap bg-[#18181b] w-full gap-x-11">
             {data.map((card, index) => (
-                <Link key={index} className="w-[27.3vh] mb-[3%] mt-3">
+                <Link key={index} className="w-[27.3vh] my-3">
                     <img className="shadow-[8px_17px_38px_2px_rgba(0,0,0,0.5)] h-[40vh] w-full object-cover" src={`https://image.tmdb.org/t/p/original/${card.poster_path || card.backdrop_path}`} alt="img"/>
                     <h1 className="text-xl text-zinc-400 mt-2 font-semibold">
                         {card.title ||
